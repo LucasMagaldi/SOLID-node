0dcbf64fb504d7798e89da1fa0f3e80d1b92c35c
Lucas Magaldi <lucas.magaldi@hotmail.com>
2026-07-09 00:03:45 -0300

    chore: configure eslint and project rules

---
+++ eslint.config.ts
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["node_modules", "build"]
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js }, extends: ["js/recommended"], 
+    languageOptions: { globals: globals.node } 
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "no-duplicate-imports": "error",
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+    }
+  }
+]);

7f143774a8821722e6d5dbf96dbbf6ce5d418bbe
Lucas Magaldi <lucas.magaldi@hotmail.com>
2026-08-20 13:51:42 -0300

    feat: implement dependency inversion into register usecase

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from "fastify"; 
 import z from "zod";
-import { registerUseCase } from "../../use-cases/register.use-case";
+import { RegisterUseCase } from "../../use-cases/register.use-case";
+import { UserRepositoryPrisma } from "../../repositories/prisma/user-repository.prisma";
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
     const registerUserInputDTO = z.object({
@@ -10,9 +11,11 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
     });    
     
     const { name, email, password } = registerUserInputDTO.parse(req.body);
+    const userRepository = new UserRepositoryPrisma()
+    const registerUseCase = new RegisterUseCase(userRepository)
    
     try {
-        await registerUseCase({
+        await registerUseCase.execute({
             name,
             email,
             password,
